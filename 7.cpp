b04c3b24c5d58c4f513fb59668619773d6f83d1f
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:58:01 -0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,3 +1,8 @@
+/*
+7. Faça uma função que verifique se um valor é perfeito ou não. Um valor é dito perfeito quando ele é igual a soma 
+dos seus divisores excetuando ele próprio. (Ex: 6 é perfeito, 6 = 1 + 2 + 3, que são seus divisores). 
+A função deve retornar um valor booleano.
+*/
 #include <iostream>
 
 using namespace std;
