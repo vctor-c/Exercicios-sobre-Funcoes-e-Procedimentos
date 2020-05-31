063c040f8eab023869188aee4764b84879ae03f1
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:59:56 -0300

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -1,3 +1,7 @@
+/*
+9. Faça uma função que recebe um valor inteiro e verifica se o valor é positivo ou negativo. 
+A função deve retornar um valor booleano.
+*/
 #include <iostream>
 
 using namespace std;
