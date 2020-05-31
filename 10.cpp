9e369c306beac599bc3a1d2a30f5a7bec22bbda9
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-31 00:00:55 -0300

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,3 +1,8 @@
+/*
+10. Faça uma função que recebe um valor inteiro e verifica se o valor é par ou ímpar. 
+A função deve retornar um valor booleano.
+*/
+
 #include <iostream>
 
 using namespace std;
