291ca7e174b9a236e14e6665ee8b20d2089c0731
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:56:02 -0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,3 +1,7 @@
+/*
+4. Faça um procedimento que recebe por parâmetro os valores necessário para o cálculo da fórmula de báskara e retorna, também 
+por parâmetro, as suas raízes, caso seja possível calcular.
+*/
 #include <iostream>
 #include<math.h>
 #include <iomanip>
