b5d9c47f71b48d773e84af972ecc7268225348e8
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:56:41 -0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,3 +1,7 @@
+/*
+5. Faça uma função que recebe por parâmetro o tempo de duração de uma fábrica expressa em segundos e retorna também por 
+parâmetro esse tempo em horas, minutos e segundos.
+*/
 #include <iostream>
 
 using namespace std;
