bef835fa048a0355c2a6080605227d4d475d00b1
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:55:17 -0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,3 +1,7 @@
+/*
+3. Faça uma função que recebe por parâmetro um valor inteiro e positivo e retorna o valor lógico Verdadeiro 
+caso o valor seja primo e Falso em caso contrário.
+*/
 #include <iostream>
 
 using namespace std;
