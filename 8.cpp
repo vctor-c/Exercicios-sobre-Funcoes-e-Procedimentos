462a10cfa867024351566d7a01f524b399086126
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:59:19 -0300

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -1,3 +1,15 @@
+/*
+8. Faça um procedimento que recebe a idade de um nadador por parâmetro e retorna , também por parâmetro, 
+a categoria desse nadador de acordo com a tabela abaixo:  
+
+Idade	    Categoria
+5 a 7 anos	Infantil A
+8 a 10 anos	Infantil B
+11-13 anos	Juvenil A
+14-17 anos	Juvenil B
+Maiores de      Adulto
+18 anos (inclusive)
+*/
 #include <iostream>
 
 using namespace std;
