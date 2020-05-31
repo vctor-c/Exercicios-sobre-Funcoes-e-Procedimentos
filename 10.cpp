6ec8007dd791502036e7cd8528850b2ca5ef67c7
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:51:55 -0300

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+bool posneg (int x){
+
+    if (x%2==0){
+    return true;
+    }else{
+    return false;
+    }
+}
+
+int main (){
+    int x;
+    bool r;
+
+    cin >> x;
+
+    r=posneg(x);
+
+    if (r==1){
+    cout << "Par" << endl;
+    }else if (r==0) {
+    cout << "Impar" << endl;
+    }
+
+return 0;
+}
