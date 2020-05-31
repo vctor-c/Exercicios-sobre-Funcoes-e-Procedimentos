02a021b2f3e7dff04e5c0acedb0a8d6855eb8849
vctor-c <65521783+vctor-c@users.noreply.github.com>
2020-05-30 23:54:38 -0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,9 @@
+/*
+2. Escreva um procedimento que recebe as 3 notas de um aluno por parâmetro e uma letra. 
+Se a letra for A o procedimento calcula a média aritmética das notas do aluno, se for P, a sua média ponderada 
+(pesos: 5, 3 e 2) e se for H, a sua média harmônica. A média calculada também deve retornar por parâmetro.
+*/
+
 #include <iostream>
 #include <ctype.h>
 #include <iomanip>
